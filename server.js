154b86a07ea9c6c6e753b476f608b78d50aa72b8
john <jgarza@unomaha.edu>
2019-11-10 22:06:42 -0600

    push 3

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-function require(express1) {
-  return undefined;
-}
-
 // server.js
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 app = express();
-let __dirname;
 app.use(serveStatic(__dirname + "/dist"));
-let process;
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
